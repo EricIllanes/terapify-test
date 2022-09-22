8abab12f312048dacbd71e3e2be9c527ab7ee426
EricIllanes <e.icuriqueo@gmail.com>
2022-09-22 00:49:31 -0300

    update

--- src/routes/Appointment.js
+++ src/routes/Appointment.js
@@ -4,6 +4,7 @@ const Client = require("../models/Client.js");
 const Psy = require("../models/Psy.js");
 const Hours = require("../models/Hours.js");
 const Appointment = require("../models/Appointment.js");
+const e = require("express");
 
 router.get("/appointment", async (req, res) => {
   try {
@@ -21,15 +22,21 @@ router.post("/appointment", async (req, res) => {
   const psyFind = await Psy.findById(psyID);
   const clientFind = await Client.findById(clientID);
   let checkAvailable = await Hours.find({
-    schedule: { $elemMatch: { day: date, intHour } },
+    schedule: { $elemMatch: { day: date, intHour:hour } },
   });
-  console.log("****", checkAvailable);
+  console.log('Probanding', checkAvailable)
+ if(checkAvailable.length===0){
+  res.status(404).send('No hay horas disponibles en ese horario, intenta con otro')
+ } else {
   // const appointment= new Appointment({date, hour, psy:psyFind._id, client:clientFind._id})
   // await appointment.save()
   // clientFind.appointment= clientFind.appointment.concat(appointment._id)
   // await clientFind.save()
   // res.json(appointment)
-  res.send("Probanding");
+  res.send(checkAvailable);
+
+ }
+
 });
 
 module.exports = router;
